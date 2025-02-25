6958b0f0ca6650008dfa3b7a318637b86033c5c2
AlvaroQP <alonsol1569@gmail.com>
2025-02-25 13:05:07 +0100

    25-02-2025

---
+++ Northwind3/FormLogin.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Northwind3
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.lbTituloLogin = new System.Windows.Forms.Label();
+            this.panelContainer = new System.Windows.Forms.Panel();
+            this.lbPwd = new System.Windows.Forms.Label();
+            this.btnValidarUsuario = new System.Windows.Forms.Button();
+            this.tbPwd = new System.Windows.Forms.TextBox();
+            this.tbUsuario = new System.Windows.Forms.TextBox();
+            this.lbUsuario = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panelTop.SuspendLayout();
+            this.panelContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panelContainer);
+            this.panel1.Controls.Add(this.panelTop);
+            // 
+            // panelTop
+            // 
+            this.panelTop.Controls.Add(this.lbTituloLogin);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(800, 79);
+            this.panelTop.TabIndex = 0;
+            // 
+            // lbTituloLogin
+            // 
+            this.lbTituloLogin.AutoSize = true;
+            this.lbTituloLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTituloLogin.Location = new System.Drawing.Point(323, 24);
+            this.lbTituloLogin.Name = "lbTituloLogin";
+            this.lbTituloLogin.Size = new System.Drawing.Size(96, 37);
+            this.lbTituloLogin.TabIndex = 0;
+            this.lbTituloLogin.Text = "Login";
+            this.lbTituloLogin.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panelContainer
+            // 
+            this.panelContainer.Controls.Add(this.lbPwd);
+            this.panelContainer.Controls.Add(this.btnValidarUsuario);
+            this.panelContainer.Controls.Add(this.tbPwd);
+            this.panelContainer.Controls.Add(this.tbUsuario);
+            this.panelContainer.Controls.Add(this.lbUsuario);
+            this.panelContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContainer.Location = new System.Drawing.Point(0, 79);
+            this.panelContainer.Name = "panelContainer";
+            this.panelContainer.Size = new System.Drawing.Size(800, 371);
+            this.panelContainer.TabIndex = 2;
+            // 
+            // lbPwd
+            // 
+            this.lbPwd.AutoSize = true;
+            this.lbPwd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPwd.Location = new System.Drawing.Point(241, 101);
+            this.lbPwd.Name = "lbPwd";
+            this.lbPwd.Size = new System.Drawing.Size(92, 20);
+            this.lbPwd.TabIndex = 5;
+            this.lbPwd.Text = "Contraseña";
+            // 
+            // btnValidarUsuario
+            // 
+            this.btnValidarUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnValidarUsuario.Location = new System.Drawing.Point(245, 172);
+            this.btnValidarUsuario.Name = "btnValidarUsuario";
+            this.btnValidarUsuario.Size = new System.Drawing.Size(252, 31);
+            this.btnValidarUsuario.TabIndex = 4;
+            this.btnValidarUsuario.Text = "Ingresar";
+            this.btnValidarUsuario.UseVisualStyleBackColor = true;
+            this.btnValidarUsuario.Click += new System.EventHandler(this.btnValidarUsuario_Click);
+            // 
+            // tbPwd
+            // 
+            this.tbPwd.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPwd.Location = new System.Drawing.Point(245, 124);
+            this.tbPwd.Name = "tbPwd";
+            this.tbPwd.Size = new System.Drawing.Size(252, 24);
+            this.tbPwd.TabIndex = 3;
+            // 
+            // tbUsuario
+            // 
+            this.tbUsuario.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbUsuario.Location = new System.Drawing.Point(245, 66);
+            this.tbUsuario.Name = "tbUsuario";
+            this.tbUsuario.Size = new System.Drawing.Size(252, 24);
+            this.tbUsuario.TabIndex = 2;
+            // 
+            // lbUsuario
+            // 
+            this.lbUsuario.AutoSize = true;
+            this.lbUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUsuario.Location = new System.Drawing.Point(241, 43);
+            this.lbUsuario.Name = "lbUsuario";
+            this.lbUsuario.Size = new System.Drawing.Size(64, 20);
+            this.lbUsuario.TabIndex = 0;
+            this.lbUsuario.Text = "Usuario";
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "FormLogin";
+            this.Text = "FormLogin";
+            this.panel1.ResumeLayout(false);
+            this.panelTop.ResumeLayout(false);
+            this.panelTop.PerformLayout();
+            this.panelContainer.ResumeLayout(false);
+            this.panelContainer.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Label lbTituloLogin;
+        private System.Windows.Forms.Panel panelContainer;
+        private System.Windows.Forms.Label lbPwd;
+        private System.Windows.Forms.Button btnValidarUsuario;
+        private System.Windows.Forms.TextBox tbPwd;
+        private System.Windows.Forms.TextBox tbUsuario;
+        private System.Windows.Forms.Label lbUsuario;
+    }
+}
---
+++ Northwind3/FormLogin.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Northwind3
+{
+    public partial class FormLogin : FormBase
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValidarUsuario_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection("Server=localhost\\SQLEXPRESS;Database=Northwind;Integrated Security=True;TrustServerCertificate=True");
+                conn.Open();
+                String query = $"SELECT * FROM NCLusuario WHERE nombre_usuario = '{tbUsuario.Text}' AND pwd_usuario = '{tbPwd.Text}'";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.HasRows) {
+                    Boolean estaUsuarioBloqueado = false; 
+
+                    while (reader.Read()) { 
+                        estaUsuarioBloqueado = Convert.ToBoolean(reader["bloqueado"]);
+                    }
+                    reader.Close();
+
+                    if (!estaUsuarioBloqueado)
+                    {
+                        // Insertar en la tabla de sesion el intento exitoso
+                        insertarIntentoSesion(conn, tbUsuario.Text, true);
+                        Global.userLogin = tbUsuario.Text;
+                        DialogResult = DialogResult.OK;
+                    }
+                    else
+                    {
+                        Console.WriteLine("El usuario esta bloqueado");
+                    }
+                }
+                else
+                {
+                    // Comprobamos que el usuario exista para poder insertar en la tabla de sesiones los intentos y bloquear la cuenta si es necesario
+                    reader.Close();
+
+                    query = $"SELECT * FROM NCLusuario WHERE nombre_usuario = '{tbUsuario.Text}'";
+                    cmd = new SqlCommand(query, conn);
+                    reader = cmd.ExecuteReader();
+                    if (reader.HasRows) {
+                        Boolean estaUsuarioBloqueado = false;
+
+                        while (reader.Read())
+                        {
+                            estaUsuarioBloqueado = Convert.ToBoolean(reader["bloqueado"]);
+                        }
+                        reader.Close();
+
+                        if (!estaUsuarioBloqueado)
+                        {
+                            insertarIntentoSesion(conn, tbUsuario.Text, false);
+                            int resultado = VerificarNumeroIntentos(conn, tbUsuario.Text);
+                            if(resultado == 1)
+                            {
+                                BloquearUsuario(conn, tbUsuario.Text);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("El usuario esta bloqueado");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+
+        public static void BloquearUsuario(SqlConnection conn, string nombreUsuario)
+        {
+            using (SqlCommand cmd = new SqlCommand("BloquearUsuario", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@nombreUsuario", nombreUsuario);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public static void insertarIntentoSesion(SqlConnection conn, string usuario, bool estado)
+        {
+            using (SqlCommand cmd = new SqlCommand("InsertarIntentoSesion", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@nombreUsuario", usuario);
+                cmd.Parameters.AddWithValue("@intentoEstado", estado);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public static int VerificarNumeroIntentos(SqlConnection conn, String nombreUsuario)
+        {
+            string query = "SELECT dbo.VerificarIntento(@nombreUsuario)";
+            int resultadoFinal = -1;
+
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@nombreUsuario", nombreUsuario);
+                var resultado = cmd.ExecuteScalar();
+
+                if (resultado != null)
+                {
+                    resultadoFinal = Convert.ToInt32(resultado);
+                }
+
+            }
+
+            return resultadoFinal;
+        }
+
+    }
+
+
+}
