4e398d6e75896d764c4fd337315e430b4b2f89e5
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2025-03-10 17:20:54 +0100

    10-03-2025

---
+++ AppConsola/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+ * Conectarse a la base de datos
+ * Genera para cada uno de los clientes numeroVentas, numeroTotalDeVentas
+ */
+Console.WriteLine("Hello, World!");
+
+
---
+++ AppConsola2/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Data.SqlClient;
+
+/*
+ * Conectarse a la base de datos
+ * Genera para cada uno de los clientes numeroVentas, numeroTotalDeVentas
+ */
+
+try
+{
+    SqlConnection conn = new SqlConnection("Server=localhost\\SQLEXPRESS;Database=Northwind;Integrated Security=True;TrustServerCertificate=True");
+    conn.Open();
+    String query = $"SELECT  c.CustomerID, (SELECT COUNT(*) FROM Orders as o WHERE o.CustomerID=c.CustomerID) as ventas FROM Customers as c ";
+    SqlCommand cmd = new SqlCommand(query, conn);
+    SqlDataReader reader = cmd.ExecuteReader();
+
+
+    if (reader.HasRows)
+    {
+        // Crear el fichero donde se guardará los datos
+        
+
+
+        
+        while (reader.Read()) {
+            string nombre = Convert.ToString(reader["CustomerID"]);
+            int numeroVentas = Convert.ToInt16(reader["ventas"]);
+            Console.WriteLine($"{nombre} - {numeroVentas}");
+        }
+
+    }
+}   
+catch ( Exception e) {  
+    Console.WriteLine(e.ToString()); 
+}
